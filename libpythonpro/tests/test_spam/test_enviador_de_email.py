326198ff544da152a687d65b8b6129f92d34f049
Kauan-Santos <kasantos.business@gmail.com>
2022-11-03 19:45:40 -0300

    Adicionado test remetente

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -4,3 +4,13 @@ from libpythonpro.spam.enviador_de_email import Enviador
 def test_criar_enviador_de_email():
     enviador= Enviador()
     assert enviador is not None
+
+
+def test_remetente():
+    enviador = Enviador()
+    resultado = enviador.enviar('kauan.santos2225@gmail.com',
+    'kasantos.business@gmail.com',
+    'Curso Python Pro',
+    'Conteudo do curso de python'
+    )
+    assert 'kauan.santos2225@gmail.com' in resultado
