047b58ded67f4547738e9468bd290b8d22a8b11c
Kauan-Santos <kasantos.business@gmail.com>
2022-11-08 21:44:01 -0300

    Configurado teste remetente

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,16 +1,36 @@
-from libpythonpro.spam.enviador_de_email import Enviador
-
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+import pytest
 
 def test_criar_enviador_de_email():
     enviador= Enviador()
     assert enviador is not None
 
 
-def test_remetente():
+@pytest.mark.parametrize(
+    'remetente',
+    ['kauan.santos2225@gmail.com', 'foo@bar.com.br']
+)
+def test_remetente(remetente):
     enviador = Enviador()
-    resultado = enviador.enviar('kauan.santos2225@gmail.com',
-    'kasantos.business@gmail.com',
-    'Curso Python Pro',
-    'Conteudo do curso de python'
+    resultado = enviador.enviar(
+        remetente,
+        'kasantos.business@gmail.com',
+        'Curso Python Pro',
+        'Conteudo do curso de python'
     )
-    assert 'kauan.santos2225@gmail.com' in resultado
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'kauan']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+            remetente,
+            'kasantos.business@gmail.com',
+            'Curso Python Pro',
+            'Conteudo do curso de python'
+        )
